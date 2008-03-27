f79cea3559c8dfdbb965e7758624634f5f6959da
James Rowe <jnrowe@ukfsn.org>
2008-03-27 07:20:52 +0000

    Only specify test package metadata in the __init__ file.

--- test/mock.py
+++ test/mock.py
@@ -21,25 +21,6 @@ mock - Mock objects for doctest code snippets
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-__version__ = "0.1.0"
-__date__ = "2007-11-21"
-__author__ = "James Rowe <jnrowe@ukfsn.org>"
-__copyright__ = "Copyright (C) 2007-2008 James Rowe"
-__license__ = "GNU General Public License Version 3"
-__credits__ = ""
-__history__ = "See Mercurial repository"
-
-from email.utils import parseaddr
-
-__doc__ += """
-
-:version: %s
-:author: `%s <mailto:%s>`__
-:copyright: %s
-:status: WIP
-:license: %s
-""" % ((__version__, ) + parseaddr(__author__) + (__copyright__, __license__))
-
 import __builtin__
 import os
 import urllib
